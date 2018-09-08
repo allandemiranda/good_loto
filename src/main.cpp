fd5a9cca4d42e6121f3db9106ff7abef43769106
Allan de Miranda <allandemiranda@gmail.com>
2018-09-08 12:33:38 -0300

    desenvolvendo o main

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,18 @@
  * @brief Função que irá gerar os jogos possiveis
  * 
  * @file main.cpp
- * @author Allan de Miranda
- * @date 2018-09-08
+ * @author Allan de Miranda Silva
+ * @date 08-09-2018
  */
+#include <iostream>
+#include "analise/numeros_sorteados.h"
+
+int main(void){
+    // Verificar menor e amiores números que saem em suas posições
+    for(int posicao(1); posicao<=15; ++posicao){
+        for(auto *i = std::begin(numeros_sorteados); i<std::end(numeros_sorteados); ++i){
+
+        }
+    }
+    return 0;
+}
