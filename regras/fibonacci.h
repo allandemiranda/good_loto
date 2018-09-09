ba2fc1e26f0317b061b3261102ca428b5f0881b2
Allan de Miranda <allandemiranda@gmail.com>
2018-09-09 10:48:07 -0300

    nova regra feira

--- regras/fibonacci.h
+++ regras/fibonacci.h
@@ -9,28 +9,42 @@
 #include <iterator>
 
 bool fibonacci_binario_soma(int *inicial){
+    // primeiro definição
     int numeros_p_analise[] = {1,2,3,5,8,13,21}; // números Fibonacci da cartela
-    int tamanho = std::distance(std::begin(numeros_p_analise), std::end(numeros_p_analise));
     int soma(0);
-    for(int i(0); i<15; ++i){
-        for(int j(0); j<tamanho; ++j){
-            if(*(i+inicial) == *(j+inicial)){
-                soma += *(i+inicial);
-                break; // pq é 15
+    for(int *i=inicial; i<(inicial+15); ++i){
+        for(int *j=std::begin(numeros_p_analise); j<std::end(numeros_p_analise); ++j){
+            if(*i == *j){
+                soma += *i;
+                break;
             }
         }
-    }   
-    // A cada 15 números a soma dos números fibonacci que sairão nunca será 0, 1, 2, 4, 5, 7, 12 ou maiores que 53
-    if((soma < 3) or (soma == 4) or (soma == 5) or (soma == 7) or (soma == 12) or (soma > 53)){
+    }
+    if((soma == 0) or (soma == 1) or (soma == 2) or (soma == 4) or (soma == 5) or (soma == 7) or (soma == 12) or (soma > 53)){
+        return false;
+    } 
+
+    int quantidade_de_jogos_sorteados = std::distance(std::begin(numeros_sorteados), std::end(numeros_sorteados)) / 15;
+    int analise_padrao;
+
+    // segunda definição
+    analise_padrao = 2;
+    if((quantidade_de_jogos_sorteados+1)%analise_padrao != 0){
+        return true;
+    }
+    int soma_analise_padrao = 0;
+    for(int *i=((std::begin(numeros_sorteados))+(15*(quantidade_de_jogos_sorteados + 2 - analise_padrao))); i<std::end(numeros_sorteados); ++i){
+        for(int *j=std::begin(numeros_p_analise); j<std::end(numeros_p_analise); ++j){
+            if(*i == *j){
+                soma_analise_padrao += *i;
+                break;
+            }
+        }
+    }
+    if(((soma + soma_analise_padrao) < 20) or ((soma + soma_analise_padrao) > 100)){
         return false;
     }
 
-    //checar o ciclo para saber se pode analisar
-    int verificar_para_ciclo = 21;
-    int tamanho_sorteados = std::distance(std::begin(numeros_sorteados), std::end(numeros_sorteados));
-    int q_jogos_sorteados = tamanho_sorteados / verificar_para_ciclo;
-    if((q_jogos_sorteados*verificar_para_ciclo)+15)
-
-    
-
+    //fim
+    return true;  
 }
