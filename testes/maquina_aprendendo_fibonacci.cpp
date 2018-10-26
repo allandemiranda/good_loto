8e11bca3478661d944c4b6fffabe14fe7e18b84c
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-10-26 10:29:03 -0300

    novas modificações e aprendisagem

--- testes/maquina_aprendendo_fibonacci.cpp
+++ testes/maquina_aprendendo_fibonacci.cpp
@@ -20,7 +20,7 @@
 
 int main(int argc, char const *argv[])
 {
-    int numeros_para_analise[13]; /// verificar sempre a quantidade aqui
+    int numeros_para_analise[14]; /// verificar sempre a quantidade aqui
     std::vector <int> resultados;
     for(int a = 1; a<=25; ++a){
         numeros_para_analise[0] = a;
@@ -48,6 +48,8 @@ int main(int argc, char const *argv[])
                                                     numeros_para_analise[11] = n;
                                                     for(int o = n + 1; o<=25; ++o){
                                                         numeros_para_analise[12] = o;
+                                                        for(int p = o + 1; p<=25; ++p){
+                                                            numeros_para_analise[13] = p;
 ///  *** 
 std::vector <int> analise;
 for(int *i = std::begin(numeros_sorteados); i<std::end(numeros_sorteados); i+=15){
@@ -72,6 +74,7 @@ std::cout << contador_novo << " ";
 }                
 std::cout << std::endl;
 /// ***
+                                                        }
                                                     }
                                                 }
                                             }
